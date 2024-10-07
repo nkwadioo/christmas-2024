c13d3de886c34c3ad04d2ce34cafecf4d2fd6765
CapturedPhotography <nkwadioo@gmail.com>
2024-10-07 21:36:58 +0200

    spartan-ng integration
    - test button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HlmButtonDirective} from '@spartan-ng/ui-button-helm';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HlmButtonDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [require('@spartan-ng/ui-core/hlm-tailwind-preset')],
   content: [
     "./src/**/*.{html,ts}",
+    "./components/ui/**/*.{html,ts}"
   ],
   theme: {
     extend: {},
